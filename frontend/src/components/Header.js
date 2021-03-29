e72633566cf94d132c66471674e5e3797d86e3e7
Marvin Correia <marvin.correia@devgo.org>
2021-03-29 17:09:45 -0100

    frontend update

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,22 +1,28 @@
 import React from 'react'
 import { Nav, Navbar, Form, Button, NavDropdown, FormControl, Container } from 'react-bootstrap'
-
+import { LinkContainer } from 'react-router-bootstrap'
 function Header() {
   return (
     <header>
       <Navbar bg="dark" expand="lg" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Proshop</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>Proshop</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
-              <Nav.Link href="/login"><i className="fas fa-user" ></i>Login</Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link><i className="fas fa-user" ></i>Login</Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-    </header>
+    </header >
   )
 }
 
